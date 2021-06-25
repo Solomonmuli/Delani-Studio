7b02a595189a4be07774bb8d7b639147fe216383
solo <solomon.munyalo@student.moringaschool.com>
2021-06-25 05:33:55 +0300

    update form section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 
 
-//form section
+//form 
 $(document).ready(() => {
   $("#submit").click((e) => {
     if ($("#email").val() === "" || $("#name").val() === "") {
       e.preventDefault();
       alert('enter your Name and email address');
-    } else alert(`${$("#name").val()} we have received your message. Thank you for contacting us. `);
+    } else alert(`${$("#name").val()} We have received your message. Thank you for contacting us. `);
     $("form").trigger("reset");
   });
 })
-//form section
+//form 
